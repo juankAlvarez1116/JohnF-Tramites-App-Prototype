55c99ce450ae67fac737c0ffe612a694b497fe18
Juan Camilo Alvarez <juancalvarez1116@gmail.com>
2026-05-31 17:36:44 -0500

    Add some changes

--- PrototypeProject/script.js
+++ PrototypeProject/script.js
@@ -3,6 +3,7 @@ function ingresar() {
     document.getElementById("login").style.display = "none";
 
     document.getElementById("sistema").classList.remove("oculto");
+}
 
 function cerrarSesion() {
     document.getElementById("sistema").classList.add("oculto");
@@ -11,14 +12,63 @@ function cerrarSesion() {
 
 function mostrarSeccion(id) {
 
-    let secciones = document.querySelectorAll(".seccion");
+    let secciones =
+        document.querySelectorAll(".seccion");
 
     secciones.forEach(function (seccion) {
-
         seccion.classList.add("oculto");
-
     });
 
-    document.getElementById(id).classList.remove("oculto");
+    document.getElementById(id)
+        .classList.remove("oculto");
+}
 
+function agregarCliente() {
+
+    let nombre = prompt("Nombre:");
+    let telefono = prompt("Teléfono:");
+    let correo = prompt("Correo:");
+
+    let tabla =
+        document.getElementById("tablaClientes");
+
+    let fila = tabla.insertRow();
+
+    fila.innerHTML =
+        "<td>" + nombre + "</td>" +
+        "<td>" + telefono + "</td>" +
+        "<td>" + correo + "</td>" +
+        "<td><button>Editar</button></td>";
+}
+
+function agregarVehiculo() {
+
+    let placa = prompt("Placa:");
+    let ciudad = prompt("Ciudad:");
+    let cliente = prompt("Cliente:");
+
+    let tabla =
+        document.getElementById("tablaVehiculos");
+
+    let fila = tabla.insertRow();
+
+    fila.innerHTML =
+        "<td>" + placa + "</td>" +
+        "<td>" + ciudad + "</td>" +
+        "<td>" + cliente + "</td>" +
+        "<td>Pendiente</td>" +
+        "<td class='amarillo'>Por revisar</td>" +
+        "<td>Pendiente</td>" +
+        "<td class='amarillo'>Por revisar</td>" +
+        "<td><button>Editar</button></td>";
+}
+
+function calcularCotizacion() {
+
+    document.getElementById(
+        "resultadoCotizacion"
+    ).innerHTML =
+        "Valor trámite: $250.000<br>" +
+        "Servicio JohnF: $100.000<br><br>" +
+        "<strong>Total: $350.000</strong>";
 }
